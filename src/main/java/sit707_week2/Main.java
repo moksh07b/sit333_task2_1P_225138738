b65fabb778b7dc848e75915a88b51e3341623b3c
Moksh Bansal <mokshbansal07@gmail.com>
2026-03-26 00:59:34 +1100

    second part

--- src/main/java/sit707_week2/Main.java
+++ src/main/java/sit707_week2/Main.java
@@ -8,6 +8,6 @@ public class Main
 {
     public static void main( String[] args )
     {
-        SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+        SeleniumOperations.automation_registration_page("https://automationexercise.com/signup");
     }
 }
--- src/main/java/sit707_week2/SeleniumOperations.java
+++ src/main/java/sit707_week2/SeleniumOperations.java
@@ -87,5 +87,54 @@ public class SeleniumOperations {
 		driver.close();	
 	}
 	
+		public static void automation_registration_page(String url) {
+		
+		// Step 2: Use above chrome driver to open up a chromium browser.
+		System.out.println("Fire up chrome browser.");
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("Driver info: " + driver);
+		
+		sleep(2);
+	
+		// Load a webpage in chromium browser.
+		driver.get(url);
+		
+				
+		// Find first input field which is firstname
+		
+		WebElement element = driver.findElement(By.name("name"));
+		element.sendKeys("Moksh Bansal");
+		
+		
+		element = driver.findElement(By.cssSelector("[data-qa='signup-email']"));
+		element.sendKeys("abcgmail34@gmail.com");
+		
+	
+		
+		element = driver.findElement(By.cssSelector("[data-qa='signup-button']"));
+		element.click();
+	
+		
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File("automation.png");
+		
+		try {
+			FileUtils.copyFile(source, dest);
+			System.out.println("Screenshot saved");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(dest.getAbsolutePath());
+		
+		// Sleep a while
+		sleep(2);
+		
+		// close chrome driver
+		driver.close();	
+	}
+	
 	
 }
