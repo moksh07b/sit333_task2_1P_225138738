89857713a05dcdc0963ccfc94a76455f2c48dfb7
Moksh Bansal <mokshbansal07@gmail.com>
2026-03-26 00:39:46 +1100

    Initial

---
+++ src/main/java/sit707_week2/SeleniumOperations.java
@@ -0,0 +1,91 @@
+package sit707_week2;
+
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class SeleniumOperations {
+
+	public static void sleep(int sec) {
+		try {
+			Thread.sleep(sec*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void officeworks_registration_page(String url) {
+		
+		// Step 2: Use above chrome driver to open up a chromium browser.
+		System.out.println("Fire up chrome browser.");
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("Driver info: " + driver);
+		
+		sleep(2);
+	
+		// Load a webpage in chromium browser.
+		driver.get(url);
+		
+				
+		// Find first input field which is firstname
+		WebElement element = driver.findElement(By.id("firstname"));
+		System.out.println("Found element: " + element);
+		// Send first name
+		element.sendKeys("Moksh");
+		
+		element = driver.findElement(By.id("lastname"));
+		
+		element.sendKeys("Bansal");
+		
+		element = driver.findElement(By.id("phoneNumber"));
+		element.sendKeys("0403345678");
+		
+		element = driver.findElement(By.id("email"));
+		element.sendKeys("abcgmail34@gmail.com");
+		
+		element = driver.findElement(By.id("password"));
+		element.sendKeys("123456798");
+		
+		element = driver.findElement(By.id("confirmPassword"));
+		element.sendKeys("123456798");
+		
+		element = driver.findElement(By.className("iJIqgV"));
+		element.click();
+		
+		/*
+		 * Take screenshot using selenium API.
+		 */
+		// Write code
+		
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File("officeworks.png");
+		
+		try {
+			FileUtils.copyFile(source, dest);
+			System.out.println("Screenshot saved");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(dest.getAbsolutePath());
+		
+		// Sleep a while
+		sleep(2);
+		
+		// close chrome driver
+		driver.close();	
+	}
+	
+	
+}
